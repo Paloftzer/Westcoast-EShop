4a8e10ebb89196e5469b73ca72f0d5ae3d74297b
Paloftzer <phille04@outlook.com>
2024-09-30 16:00:37 +0200

    Created a Customer class

---
+++ models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace Westcoast_EShop.Models;
+
+public class Customer
+{
+    public int CustomerId { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime LastPurchase { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? AddressLine { get; set; }
+    public string? PostalCode { get; set; }
+    public string? City { get; set; }
+}
